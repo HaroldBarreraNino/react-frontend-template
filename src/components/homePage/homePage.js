a454f1fd4b4037f8194531c3fa73a896f07771ca
HaroldBarreraNino <hsbarreran@outlook.com>
2022-12-19 15:53:30 -0500

    Initial Commit

---
+++ src/components/homePage/homePage.js
@@ -0,0 +1,11 @@
+const HomePage = () => {
+    return (
+        <>
+            <header>
+                <h1>BIBLIOTECA ONLINE</h1>
+            </header>
+        </>
+    );
+}
+
+export default HomePage;

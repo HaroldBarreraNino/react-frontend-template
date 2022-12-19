fa42182814fa35c8b3eac6f247735f08bbaed432
HaroldBarreraNino <hsbarreran@outlook.com>
2022-12-19 16:25:28 -0500

    home page

--- src/components/homePage/homePage.js
+++ src/components/homePage/homePage.js
@@ -1,9 +1,15 @@
+import Navbar from "../navbarComponent/navbar";
+import Footer from "../footerComponent/footer";
+
 const HomePage = () => {
     return (
         <>
             <header>
-                <h1>BIBLIOTECA ONLINE</h1>
+                <h1>BIBLIOTECA ONLINE</h1>  
             </header>
+            <Navbar />
+            <div id="banner"></div>
+            <Footer />
         </>
     );
 }
